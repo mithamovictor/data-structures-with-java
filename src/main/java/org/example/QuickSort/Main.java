bf22b4722bde6bd1413818d8c93aefa5a7894a74
Victor Mithamo <vickarungaru@gmail.com>
2024-04-14 17:04:59 +0300

    quick sort

--- src/main/java/org/example/QuickSort/Main.java
+++ src/main/java/org/example/QuickSort/Main.java
@@ -2,8 +2,45 @@ package org.example.QuickSort;
 
 public class Main {
     public static void main(String[] args) {
+        // Quick sort = moves smaller elements to left of a pivot.
+        // run-time complexity:
+        // - Best case: O(n log(n))
+        // - Average case O(n log(n))
+        // - Worst case O(n^2) if already sorted
 
+        // Space complexity = O(log(n)) due to recursion
 
         int[] array = {9, 1, 8, 2, 7, 3, 6, 4, 5};
+
+        quickSort(array, 0, array.length - 1);
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static void quickSort(int[] array, int start, int end) {
+        if(end <= start) return; // base case
+        int pivot = partition(array, start, end);
+        quickSort(array, start, pivot - 1);
+        quickSort(array, pivot + 1, end);
+    }
+
+    private static int partition(int[] array, int start, int end) {
+        int pivot = array[end];
+        int i = start - 1;
+        for (int j = start; j <= end - 1; j++) {
+            if (array[j] < pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        i++;
+        int temp = array[i];
+        array[i] = array[end];
+        array[end] = temp;
+        return i;
     }
 }
