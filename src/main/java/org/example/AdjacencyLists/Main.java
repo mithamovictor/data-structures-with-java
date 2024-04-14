626df30f3544dbf3a6235dffef546971a93eb5e9
Victor Mithamo <vickarungaru@gmail.com>
2024-04-14 18:58:11 +0300

    adjacency lists

---
+++ src/main/java/org/example/AdjacencyLists/Main.java
@@ -0,0 +1,27 @@
+package org.example.AdjacencyLists;
+
+public class Main {
+    // Adjacency List = an array or arraylist of linkedlists.
+    // runtime complexity to check an Edge: O(v)
+    // space complexity: O(v + e)
+
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+
+        graph.addNode(new Node('A'));
+        graph.addNode(new Node('B'));
+        graph.addNode(new Node('C'));
+        graph.addNode(new Node('D'));
+        graph.addNode(new Node('E'));
+
+        graph.addEdge(0,1);
+        graph.addEdge(1,2);
+        graph.addEdge(1,4);
+        graph.addEdge(2,3);
+        graph.addEdge(2,4);
+        graph.addEdge(4,0);
+        graph.addEdge(4,2);
+
+        graph.print();
+    }
+}
---
+++ src/main/java/org/example/AdjacencyLists/Node.java
@@ -0,0 +1,9 @@
+package org.example.AdjacencyLists;
+
+public class Node {
+    char data;
+
+    Node(char data) {
+        this.data = data;
+    }
+}
